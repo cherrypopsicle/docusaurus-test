c314181e4bf81f303acf40c92d2c9cd52308d07c
Memo Khoury <majd.khoury93@gmail.com>
2022-08-30 18:02:47 +0400

    playing around

--- test/docusaurus.config.js
+++ test/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Harbor",
+  tagline: "Go Harbor!",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Harbor",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
